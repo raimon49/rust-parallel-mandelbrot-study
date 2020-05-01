2b734bd1c6a21050cc6da7b9a8b46d058365430c
raimon <raimon49@hotmail.com>
2020-05-01 12:05:48 +0900

    Add unit test and fix error and warning

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 extern crate num;
 use num::Complex;
+use std::str::FromStr;
 
+#[allow(dead_code)]
 /// `limit` を繰り返しの上限として、`c` がマンデルブロ集合に含まれるかを判定する
 ///
 /// `c` がマンデルブロ集合に含まれないなら `Some(i)` を返す
-fn escape_time(c: Complex<f64>, limit: u32 -> Option<u32>) {
+fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
     let mut z = Complex { re: 0.0, im: 0.0 };
 
     for i in 0..limit {
@@ -15,4 +17,27 @@ fn escape_time(c: Complex<f64>, limit: u32 -> Option<u32>) {
     }
 
     None
+}
+
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
+    match s.find(separator) {
+        None => None,
+        Some(index) => {
+            match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+                (Ok(l), Ok(r)) => Some((l, r)),
+                _ => None
+            }
+        }
+    }
+}
+
+#[test]
+fn test_parse_pair() {
+    assert_eq!(parse_pair::<i32>("",        ','), None);
+    assert_eq!(parse_pair::<i32>("10,",     ','), None);
+    assert_eq!(parse_pair::<i32>(",10",     ','), None);
+    assert_eq!(parse_pair::<i32>("10,20",   ','), Some((10, 20)));
+    assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
+    assert_eq!(parse_pair::<f64>("0.5x",    'x'), None);
+    assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
 }
