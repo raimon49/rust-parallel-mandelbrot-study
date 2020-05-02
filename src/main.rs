4f7a0a85efefa129b8fe536fa5ae5011c6ee4cd4
raimon <raimon49@hotmail.com>
2020-05-02 12:05:37 +0900

    Add func 'pixel_to_point' and unit test

--- src/main.rs
+++ src/main.rs
@@ -43,3 +43,29 @@ fn test_parse_pair() {
     assert_eq!(parse_pair::<f64>("0.5x",    'x'), None);
     assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
 }
+
+/// 出力される画像のピクセル位置を取り、対応する複素平面上の点を返す。
+/// `bounds` は出力画像の幅と高さをピクセル単位で与える。
+/// `pixel` は画像上の特定ピクセルを (行, 列) ペアの形で指定する。
+/// `upper_left` と `lower_right` は、出力画像に描画する複素平面を左上と右下で指定する。
+fn pixel_to_point(bounds: (usize, usize),
+                  pixel: (usize, usize),
+                  upper_left: Complex<f64>,
+                  lower_right: Complex<f64>)
+    -> Complex<f64>
+{
+    let (width, height) = (lower_right.re - upper_left.re,
+                           upper_left.im - lower_right.im);
+    Complex {
+        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64
+    }
+}
+
+#[test]
+fn test_pixel_to_point() {
+    assert_eq!(pixel_to_point((100, 100), (25, 75),
+                              Complex { re: -1.0, im:  1.0 },
+                              Complex { re:  1.0, im: -1.0 }),
+               Complex { re: -0.5, im: -0.5 });
+}
