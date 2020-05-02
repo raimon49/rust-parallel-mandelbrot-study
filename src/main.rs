2214113bfce98ee0d73af25d7e9367a0ddd2633c
raimon <raimon49@hotmail.com>
2020-05-02 12:25:16 +0900

    Add func 'render'

--- src/main.rs
+++ src/main.rs
@@ -69,3 +69,26 @@ fn test_pixel_to_point() {
                               Complex { re:  1.0, im: -1.0 }),
                Complex { re: -0.5, im: -0.5 });
 }
+
+/// 矩形範囲のマンデルプロ集合をピクセルのバッファに描画する。
+/// 仮引数 `bounds` はバッファ `pixels` のグレースケールの値をバイトで保持する。
+/// `upper_left` と `lower_right`
+/// はピクセルバッファの左上と右下に対応する複素平面上の点を指定する。
+fn render(pixels: &mut [u8],
+          bounds: (usize, usize),
+          upper_left: Complex<f64>,
+          lower_right: Complex<f64>)
+{
+    assert!(pixels.len() == bounds.0 * bounds.1);
+
+    for row in 0 .. bounds.1 {
+        for column in 0 .. bounds.0 {
+            let point = pixel_to_point(bounds, (column, row),
+                                       upper_left, lower_right);
+            pixels[row * bounds.0 + column] = match escape_time(point, 255) {
+                None => 0,
+                Some(count) => 255 - count as u8
+            };
+        }
+    }
+}
