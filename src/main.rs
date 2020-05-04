778461333e2db0a38ce267e640404b5f33c1d4fe
raimon <raimon49@hotmail.com>
2020-05-04 13:19:30 +0900

    Implement concurrent exec

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate num;
 extern crate image;
+extern crate crossbeam;
 use num::Complex;
 use std::str::FromStr;
 use image::ColorType;
@@ -33,8 +34,37 @@ fn main() {
 
     render(&mut pixels, bounds, upper_left, lower_right);
 
-    write_image(&args[1], &pixels, bounds)
-        .expect("error writing PNG file");
+    // 並列処理化のためコメントアウトして下の処理と置き換え
+    // write_image(&args[1], &pixels, bounds)
+    //     .expect("error writing PNG file");
+    let threads = 8;
+    let rows_per_band = bounds.1 / threads + 1;
+    {
+        let bands: Vec<&mut [u8]> = pixels.chunks_mut(rows_per_band * bounds.0).collect();
+        // クロージャ呼び出し
+        // 引数の型はRustコンパイラが型推論するため不要
+        crossbeam::scope(|spawner| {
+            for (i, band) in bands.into_iter().enumerate() {
+                let top = rows_per_band * i;
+                let height = band.len() / bounds.0;
+                let band_bounds = (bounds.0, height);
+                let band_upper_left = pixel_to_point(bounds,
+                                                     (0, top),
+                                                     upper_left,
+                                                     lower_right);
+                let band_lower_right = pixel_to_point(bounds,
+                                                      (bounds.0, top + height),
+                                                      upper_left,
+                                                      lower_right);
+
+                // 引数なしのクロージャ実行
+                // キーワードmoveは、このクロージャ内部で利用する変数の所有権を取得することを表明している
+                spawner.spawn(move || {
+                    render(band, band_bounds, band_upper_left, band_lower_right);
+                });
+            }
+        });
+    }
 }
 
 #[allow(dead_code)]
