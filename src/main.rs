8b83915d4f91dda2006b4a9f3f775c6f84b459bd
raimon <raimon49@hotmail.com>
2020-05-04 12:53:00 +0900

    Add func 'main' as first version

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,37 @@ use std::str::FromStr;
 use image::ColorType;
 use image::png::PNGEncoder;
 use std::fs::File;
+use std::io::Write;
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() != 5 {
+        writeln!(std::io::stderr(),
+                 "Usage: mandelbrot FILE PIXELS UPPERLEFT LOWERRIGHT")
+            .unwrap();
+        writeln!(std::io::stderr(),
+                 "Example: {} mandle.png 1000x750 -1.20,0.35 -1,20",
+                 args[0])
+            .unwrap();
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x')
+        .expect("error parsing image dimensions");
+    let upper_left = parse_complex(&args[3])
+        .expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4])
+        .expect("error parsing lower right corner point");
+
+    // マクロ呼び出しvec![v; n]で長さnのベクタを作り、vで初期化
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+
+    render(&mut pixels, bounds, upper_left, lower_right);
+
+    write_image(&args[1], &pixels, bounds)
+        .expect("error writing PNG file");
+}
 
 #[allow(dead_code)]
 /// `limit` を繰り返しの上限として、`c` がマンデルブロ集合に含まれるかを判定する
